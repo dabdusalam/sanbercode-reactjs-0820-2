eddc6168747f3a8025d95c046363279556e979c8
dabdusalam <dabdusalamrafi@gmail.com>
2020-09-05 08:02:10 +0700

    my-appp/src/Tugas-10/

---
+++ my-appp/src/Tugas-10/tugas10.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class tugas10 extends React.Component{
+	render(){
+		return(
+			<>
+let dataHargaBuah = [
+  {nama: "Semangka", harga: 10000, berat: 1000},
+  {nama: "Anggur", harga: 40000, berat: 500},
+  {nama: "Strawberry", harga: 30000, berat: 400},
+  {nama: "Jeruk", harga: 30000, berat: 1000},
+  {nama: "Mangga", harga: 30000, berat: 500}
+]
+			</>
+			)
+	}
+}
