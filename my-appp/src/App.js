c5d730dd060d08a140ef2bda9cdc58c04e5bdfcd
dabdusalam <dabdusalamrafi@gmail.com>
2020-09-10 06:46:26 +0700

    src

--- my-appp/src/App.js
+++ my-appp/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Timer from './Tugas-11/Timer';
+import tugas12 from './Tugas-12/tugas12';
+import DaftarBuah from './Tugas-12/DaftarBuah';
 import './App.css';
 
 function App() {
   return (
     <>
 
-      <Timer/>
+      {/*<Timer/>*/}
+      <DaftarBuah/>
     </>
   );
 }
--- my-appp/src/Tugas-11/tugas11.js
+++ my-appp/src/Tugas-11/tugas11.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-class Timer extends Component{
+class timer extends Component{
   constructor(props){
     super(props)
     this.state = {
